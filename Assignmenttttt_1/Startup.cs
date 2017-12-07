9e4f674bc0d449964daafb4e91f501d74e54e2a6
deborahmichelle <deborah.michelle41@yahoo.com>
2017-12-07 06:55:38 -0800

    new_project

---
+++ Assignmenttttt_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignmenttttt_1.Startup))]
+namespace Assignmenttttt_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
